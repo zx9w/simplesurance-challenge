6815bc62443558b3d81ece1619d3ddac1e2dded7
zx9w <ilmu@rishi.is>
2019-06-26 18:44:14 +0000

    Cleanup of comments and debug prints.

--- challenge.go
+++ challenge.go
@@ -11,17 +11,13 @@ import (
 )
 
 func main() {
-	// ------ HTTP ------
 	http.HandleFunc("/", SolutionServer)
 	http.ListenAndServe(":8080", nil)
-
-	//*/
 }
 
-// ----- Read from file -----> Side effects!
 func ReadData(filename string) []byte {
-	// We won't worry about the file being to big
-	// Since it only contains 60 seconds of requests
+	// We won't worry about the file being too big
+	// since it only contains 60 seconds of requests
 	content, Rerr := ioutil.ReadFile(filename)
 	if Rerr != nil {
 		log.Fatal(Rerr)
@@ -29,7 +25,6 @@ func ReadData(filename string) []byte {
 	return content
 }
 
-// ------ Write to file ------> Side effects!
 func WriteData(filename string, message []byte) {
 	Werr := ioutil.WriteFile(filename, message, 0644)
 	if Werr != nil {
@@ -37,17 +32,16 @@ func WriteData(filename string, message []byte) {
 	}
 }
 
-// ------ HTTP Server ------> Side effects!
 func SolutionServer(w http.ResponseWriter, r *http.Request) {
 	layout := "Mon Jan 2 15:04:05 MST 2006  (MST is GMT-0700)"
 
 	content := ReadData("date.txt")
-	fmt.Printf("Dates:\n%s\n", content)
+	//	fmt.Printf("Dates:\n%s\n", content)
 
 	lines := strings.Split(string(content[:]), "\n")
 
 	nowt := time.Now()
-	fmt.Printf("Time now:\n-> %s\n\n", nowt.Format(layout))
+	//	fmt.Printf("Time now:\n-> %s\n\n", nowt.Format(layout))
 
 	counter := 0
 	write := nowt.Format(layout)
@@ -59,32 +53,12 @@ func SolutionServer(w http.ResponseWriter, r *http.Request) {
 			write = write + "\n" + oldt.Format(layout)
 			counter += 1
 		} else {
-			fmt.Printf("Discarded: %s\n", oldt.Format(layout))
+			//			fmt.Printf("Discarded: %s\n", oldt.Format(layout))
 		}
 	}
 
 	fmt.Fprintf(w, "Number of calls in last 60 seconds: %s", strconv.Itoa(counter))
 
 	WriteData("date.txt", []byte(write))
-
-}
-
-// ------ HTTP Server ------> Side effects!
-func HelloServer(w http.ResponseWriter, r *http.Request) {
-	content := ReadData("data.txt")
-	fmt.Printf("File contents: %s\n", content)
-
-	fmt.Fprintf(w, "File contents: %s!", content) //r.URL.Path[1:])
-
-	// ------ Type conversions ----
-	i, Cerr := strconv.ParseInt(strings.TrimSpace(string(content[:])), 10, 64)
-	if Cerr != nil {
-		log.Fatal(Cerr)
-	}
-
-	message := []byte(strconv.FormatInt(i+1, 10))
-	// ============================
-
-	WriteData("data.txt", message)
 
 }
--- readme.md
+++ readme.md
@@ -3,19 +3,33 @@
 Using only the standard library, create a Go HTTP server that on each request responds with a counter of the total number of requests that it has received during the previous 60 seconds (moving window). The server should continue to the return the correct numbers after restarting it, by persisting data to a file.
 
 # Todo list
+
 [X] Read from file
+
 -> [X] Readlines
+
 [X] Write to file
+
 -> [X] I only need the datetime stamps in the file.
+
 -> [X] String builder
+
 [X] Answer GET request
+
 -> [X] Elaborate the structures
+
 [X] Date and time
+
 -> [X] Query system time
+
 -> [X] Write as string
+
 -> [X] Parse string
+
 [X] Put it all together
+
 -> [X] Learn how to make functions (^.^)
+
 -> [X] Write to the top of the file
 
 
