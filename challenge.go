2de7ddd38e423267f1322895cc20b11c4be98104
zx9w <ilmu@rishi.is>
2019-06-26 16:43:21 +0000

    I can now read int from file, increment and write.

--- challenge.go
+++ challenge.go
@@ -4,13 +4,33 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	content, err := ioutil.ReadFile("data.txt")
+	content, Rerr := ioutil.ReadFile("data.txt")
+	if Rerr != nil {
+		log.Fatal(Rerr)
+	}
+
+	fmt.Printf("File contents: %s\n", content)
+
+	i, err := strconv.ParseInt(strings.TrimSpace(string(content[:])), 10, 64)
+
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("File contents: %s", content)
+	//fmt.Printf("Incremented file contents: %s\n", strconv.FormatInt(i+1, 16))
+
+	message := []byte(strconv.FormatInt(i+1, 10))
+	//message := []byte("4")
+
+	Werr := ioutil.WriteFile("data.txt", message, 0644)
+
+	if Werr != nil {
+		log.Fatal(Werr)
+	}
+	//*/
 }
